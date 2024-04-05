70caa37da9d3b9d8d299e8a24666d986319bf180
Kourtney Smith <klsmith5370@gmail.com>
2024-04-05 16:15:47 -0400

    Added apollo package for setting up with GraphQL

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon.vercel.app/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
